c24257ddec5259e3161ca71bc31e8bab59c2f045
Mahmoud412 <mahmoudashour219@gmail.com>
2022-10-24 22:25:17 +0300

    added store

--- App.js
+++ App.js
@@ -8,14 +8,12 @@ import HomeScreen from './src/screens/Home/HomeScreen';
 
 import { store } from './src/redux/store';
 import { Provider } from 'react-redux';
-import { ApiProvider } from '@reduxjs/toolkit/query/react';
-import { apiSlice } from './src/redux/api/apiSlice';
 
 const App = () => {
   return (
-    <ApiProvider api={apiSlice}>
+    <Provider store={store}>
     <HomeScreen/>
-    </ApiProvider>
+    </Provider>
    
   );
 };
