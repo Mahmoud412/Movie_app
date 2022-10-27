3083a63c61eaa54c84c84a974dc6ceed3f1e15e0
Mahmoud412 <mahmoudashour219@gmail.com>
2022-10-27 19:41:12 +0300

    added movie details

--- src/components/MovieDetails/PopularMovieDetail.js
+++ src/components/MovieDetails/PopularMovieDetail.js
@@ -1,13 +1,32 @@
-import {View, Text, Image, StyleSheet} from 'react-native';
+import {View, Text, Image, StyleSheet, TouchableOpacity,ScrollView} from 'react-native';
 import React, {useState, useEffect} from 'react';
 import {GET} from '../../Services/API';
 import {IMAGE_POSTER_URL} from '../../config';
 import Loader from '../Loader';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
+
 
 const PopularMovieDetail = ({movieId}) => {
   const id = movieId;
   const [loading, setLoading] = useState(true);
   const [details, setDetails] = useState([]);
+  const [videos ,setVideos ] = useState([])
+  console.log(videos)
+  const tofix = Math.trunc(details.vote_average);
+  const ratings = [tofix];
+  const starArray = [...Array(10).keys()].map(i => i + 1);
+  const Rating = ({rating}) =>
+    starArray.map(i => (
+      <FontAwesome
+        key={i}
+        name="star"
+        size={25}
+        color={rating >= i ? '#f3be11' : 'lightgrey'}
+      />
+    ));
 
   useEffect(() => {
     const getDetails = async () => {
@@ -16,11 +35,19 @@ const PopularMovieDetail = ({movieId}) => {
       setLoading(false);
     };
 
+    const getVideos = async ()=>{
+      const video = await GET(`/movie/${id}/videos`);
+      setVideos(video.results)
+
+    }
+
     getDetails();
+    getVideos();
+
   }, []);
 
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       {loading ? (
         <Loader />
       ) : (
@@ -34,15 +61,42 @@ const PopularMovieDetail = ({movieId}) => {
               style={{width: '50%', height: 300}}
               source={{uri: `${IMAGE_POSTER_URL}${details.poster_path}`}}
             />
-            <View>
-              <Text>{details.release_date}</Text>
-              <Text>{details.title}</Text>
-              <Text>{details.runtime} Minutes</Text>
+            <View style={styles.textView}>
+              <Text style={styles.release_date}> {details.release_date} </Text>
+              <Text   style={styles.title}>{details.title}</Text>
+              <View style={styles.runtimeOver}>
+                <Ionicons name="time-outline" size={25} color="#f3be11" />
+                <Text style={styles.runtime}> {details.runtime} Minutes</Text>
+              </View>
             </View>
           </View>
+          <View style={styles.sectionTwo}>
+          <View>
+              <Text style={styles.vote_average}>{tofix}</Text>
+            </View>
+          <View style={{marginLeft:10}}>
+                {ratings.map((rating, i) => (
+                  <View key={i}>
+                    <Text>
+                      <Rating rating={rating} />
+                    </Text>
+                  </View>
+                ))}
+                <Text style={styles.vote_count}>{details.vote_count} VOTES</Text>
+              </View>
+          </View>
+          <TouchableOpacity style={styles.button}>
+          <AntDesign name='heart' size={25} color='white'/>
+
+          <Text style={styles.buttonText}> ADD TO FAVORITES </Text>
+          </TouchableOpacity>
+        <View style={styles.overviewContainer}>
+        <Text  style={styles.overViewTilte}>OVERVIEW</Text>
+        <Text style={styles.overViewText}>{details.overview}</Text>
+        </View>
         </View>
       )}
-    </View>
+    </ScrollView>
   );
 };
 
@@ -51,17 +105,104 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 300,
   },
-  posterImage:{
-    width: '50%', 
+  posterImage: {
+    width: '50%',
     height: 300,
   },
-  container:{
-    backgroundColor:'#1e2634',
-    height:'100%'
+  container: {
+    backgroundColor: '#151c25',
+    height: '100%',
   },
   subContainer: {
     flexDirection: 'row',
+    padding: 15,
+  },
+  textView: {
+    margin: 10,
+  },
+  release_date: {
+    color: 'white',
+    fontWeight: 'bold',
+    marginVertical: 5,
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: '#f3be11',
+    fontSize: 16,
+    height: 30,
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 20,
+    marginVertical: 10,
+    
+    
+  },
+  runtimeOver: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  runtime: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 18,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  sectionTwo: {
+    margin: 10,
+    flexDirection:'row',
+    alignItems:'center'
+  },
+  vote_average: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 40,
+  },
+  vote_count:{
+    color:'#636c81',
+    fontWeight:'bold',
+    fontSize:18
+
+  },
+  button:{
+    color:'white',
+    backgroundColor:'#f3be11',
+    borderRadius: 20,
+    borderWidth: 1,
+    marginRight: 40,
+    marginLeft: 40,
+    marginTop: 10,
+    paddingTop: 20,
+    paddingBottom: 20,
+    flexDirection:'row',
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  buttonText:{
+    color:'white',
+    textAlign:'center',
+    fontSize:16,
+    fontWeight:'bold'
+  },
+  overviewContainer:{
+    margin:10
+  },
+  overViewTilte:{
+    color:'#636c81',
+    fontWeight:'bold',
+    fontSize:18,
     marginVertical:10
   },
+  overViewText:{
+    color:'white',
+    fontWeight:'bold',
+    fontSize:16,
+    marginBottom:10
+  }
 });
 export default PopularMovieDetail;
